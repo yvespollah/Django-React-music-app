3c1a41efb80c9ca56e6f636ff03ffd5b4d403341
yvesprogrammeur <yvesprogrammeur@gmail.com>
2024-12-10 22:21:36 +0100

    modification of view fonction and urls.py

--- music_controller/api/urls.py
+++ music_controller/api/urls.py
@@ -1,9 +1,9 @@
 
 from django.urls import path
-from .views import main
+from .views import RoomView
 
 urlpatterns = [
-    path('home',main)
+    path('home',RoomView.as_view())
 ]
 
 # this is saying that if we receive a url with no path , call the main function
--- music_controller/api/views.py
+++ music_controller/api/views.py
@@ -1,11 +1,23 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from rest_framework import generics
+from .serializers import Roomserializer
+from .models import Room
+
+
+
+# from django.http import HttpResponse
 
 
 # Create your views here.
 # this is where we will write all our endpoint e.g /hello . it is founfd after the /
 
-def main(request):
-    return HttpResponse("<h1>hello</h1>")
+# def main(request):
+#     return HttpResponse("<h1>hello</h1>")
 # now we will configure the url to nkow actually how to get to this endpoint, wethwe is /hello or etc. it is done in the urls file
 # we will create a file urls.py in the api app for it to store all the url local to that app
+
+# create an api view that will alout us to view the list of room
+
+class RoomView(generics.CreateAPIView): # this will alow us to create and view room
+    queryset = Room.objects.all()
+    serializer_class = Roomserializer
